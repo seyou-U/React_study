6f4b53115a63056c9e489fe49c155b2d06cb3815
seyou-U <rikakorink54t@yahoo.co.jp>
2024-08-18 18:16:25 +0900

    クラスコンポーネントの作成

---
+++ src/AppClass.js
@@ -0,0 +1,15 @@
+import React from 'react'; //この1文が必要
+import logo from './logo.svg';
+import './App.css';
+
+// コンポーネントの定義
+class AppClass extends React.Component {
+  render() {
+    return (
+        <div className="App">
+        </div>
+    )
+  }
+}
+
+export default AppClass;
