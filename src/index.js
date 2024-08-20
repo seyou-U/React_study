a96a5aedffdb53ab57826c3fcac85d0959c5948c
seyou-U <90487620+seyou-U@users.noreply.github.com>
2024-08-20 18:40:19 +0900

    ルート要素の追加

--- src/index.js
+++ src/index.js
@@ -16,6 +16,15 @@ setInterval(() => {
     <p>現在時刻: {(new Date()).toLocaleString()}</p>
   )
 }, 1000)
+
+root.render(
+  <>
+    <p>こんにちは、世界!</p>
+    <p>はじめまして、React</p>
+  </>
+);
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
