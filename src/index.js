5fbae61dbae442f1f6a01c3261bf7a0399777ec9
seyou-U <90487620+seyou-U@users.noreply.github.com>
2024-08-20 18:46:08 +0900

    コメント追加

--- src/index.js
+++ src/index.js
@@ -24,7 +24,16 @@ root.render(
   </>
 );
 
-
+const tag = (
+  <ul>
+    {/* コメントです　　*/}
+    <li /*
+      タグ内にコメントできる
+    */>React</li>
+    <li // これも大丈夫
+      >Vue.js</li>
+  </ul>
+);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
