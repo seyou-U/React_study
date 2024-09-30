8046d98bac20b3cb6289a992f57be811b5189b20
seyou-U <90487620+seyou-U@users.noreply.github.com>
2024-09-30 10:07:36 +0900

    StoryBookの操作方法と動作について

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,6 +8,7 @@ const config = {
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
+    "@storybook/addon-viewport"
   ],
   framework: {
     name: "@storybook/react-webpack5",
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,12 +1,30 @@
+// preview.jsのスコープ範囲はアプリ全体を指している
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 /** @type { import('@storybook/react').Preview } */
 const preview = {
   parameters: {
+    // onXxxxx, handleXxxx属性がAction検知有効になる.
+    actions: { argTypesRegex: "^(on|handle)[A-Z].*" },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
     },
+    // キャンバスの背景色をlight / dark以外から指定することができる
+    // backgrounds: {
+    //   values: [
+    //     { name: 'ghostwhite', value: '#f8f8ff' },
+    //     { name: 'aquamarine', value: '#7fffd4' },
+    //     { name: 'coral', value: '#ff7f50' },
+    //   ],
+    // },
+    // 表示領域のサイズ変更
+    viewport: {
+      viewports: INITIAL_VIEWPORTS,
+      // 既定のビューポートを設定
+      defaultViewport: 'iphonex',
+    }
   },
 };
 
